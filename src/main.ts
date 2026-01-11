8b5cd34e17116c0a97a32d179d538a12a96e2cd7
Khairat Adesina <khairatadesina01@gmail.com>
2026-01-11 23:41:23 +0100

    preparing for prod

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
